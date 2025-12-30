b0686912a772bb2056130a6cc344cfea69be60e5
Tosif2230 <techie.tosif@gmail.com>
2025-12-30 12:28:01 +0530

    Update user by put  method

--- index.js
+++ index.js
@@ -3,62 +3,80 @@ import express from "express";
 const app = express();
 const port = 8080;
 
-app.listen(port,()=>{
-    console.log(`Server Is Connected by PORT,${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server Is Connected by PORT,${port}`);
+});
 //for passing the json data this is middleware
 app.use(express.json());
 
 // Data Source //
 const users = [
-    {
-        "id" : "1",
-        "firstName": "Anshika",
-        "lastName": "Agarwal",
-        "hobby": "Teaching"
-    },
-     {
-        "id" : "2",
-        "firstName": "Mohammed",
-        "lastName": "Tosif",
-        "hobby": "Coding"
-    },
-     {
-        "id" : "3",
-        "firstName": "Munnawwar",
-        "lastName": "Khan",
-        "hobby": "Gaming"
-    },
-     {
-        "id" : "4",
-        "firstName": "Bilal",
-        "lastName": "Khan",
-        "hobby": "Designer"
-    }
-]
+  {
+    id: "1",
+    firstName: "Anshika",
+    lastName: "Agarwal",
+    hobby: "Teaching",
+  },
+  {
+    id: "2",
+    firstName: "Mohammed",
+    lastName: "Tosif",
+    hobby: "Coding",
+  },
+  {
+    id: "3",
+    firstName: "Munnawwar",
+    lastName: "Khan",
+    hobby: "Gaming",
+  },
+  {
+    id: "4",
+    firstName: "Bilal",
+    lastName: "Khan",
+    hobby: "Designer",
+  },
+];
 
 // Routes //
-app.get("/",(req,res)=>{
-    res.send("Hello")
-})
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 //Fetching user data
-app.get("/users",(req,res)=>{
-    res.send(users)
-})
-
+app.get("/users", (req, res) => {
+  res.send(users);
+});
 
 //Add New User
-app.post("/user",(req,res)=>{
-    const {firstName,lastName,hobby} = req.body;
-    
-    const newUser = {
-        id: Math.floor(Math.random()*10),//math.floor use for remove decimals.
-        firstName:firstName,
-        lastName:lastName,
-        hobby:hobby
-    };
+app.post("/user", (req, res) => {
+  const { firstName, lastName, hobby } = req.body;
 
-    users.push(newUser);
-    res.send(users)
-})
+  const newUser = {
+    id: Math.floor(Math.random() * 10), //math.floor use for remove decimals.
+    firstName: firstName,
+    lastName: lastName,
+    hobby: hobby,
+  };
 
+  users.push(newUser);
+  res.send(users);
+});
+
+//Update User or any data
+app.put("/user/:id", (req, res) => {
+  const userId = req.params.id;
+
+  const user = users.find((user) => user.id == userId);
+  //Error Handling
+  if (!user) {
+    return res
+    .status(404)
+    .json({ message: "User id does not exist." });
+  }
+  const keys = Object.keys(req.body);
+
+  keys.forEach((key)=>{
+    user[key] = req.body[key]; //updating the keys
+  });
+
+  res.send(users)
+});
