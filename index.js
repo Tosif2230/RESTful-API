e05d88a106ce0bbbfc73a999e12c3cc3511c21be
Tosif2230 <techie.tosif@gmail.com>
2025-12-30 12:01:10 +0530

    Adding user by post method

--- index.js
+++ index.js
@@ -1,8 +1,64 @@
 import express from "express";
 
-const app = new express();
-const port = 3000;
+const app = express();
+const port = 8080;
 
 app.listen(port,()=>{
     console.log(`Server Is Connected by PORT,${port}`)
 })
+//for passing the json data this is middleware
+app.use(express.json());
+
+// Data Source //
+const users = [
+    {
+        "id" : "1",
+        "firstName": "Anshika",
+        "lastName": "Agarwal",
+        "hobby": "Teaching"
+    },
+     {
+        "id" : "2",
+        "firstName": "Mohammed",
+        "lastName": "Tosif",
+        "hobby": "Coding"
+    },
+     {
+        "id" : "3",
+        "firstName": "Munnawwar",
+        "lastName": "Khan",
+        "hobby": "Gaming"
+    },
+     {
+        "id" : "4",
+        "firstName": "Bilal",
+        "lastName": "Khan",
+        "hobby": "Designer"
+    }
+]
+
+// Routes //
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+//Fetching user data
+app.get("/users",(req,res)=>{
+    res.send(users)
+})
+
+
+//Add New User
+app.post("/user",(req,res)=>{
+    const {firstName,lastName,hobby} = req.body;
+    
+    const newUser = {
+        id: Math.floor(Math.random()*10),//math.floor use for remove decimals.
+        firstName:firstName,
+        lastName:lastName,
+        hobby:hobby
+    };
+
+    users.push(newUser);
+    res.send(users)
+})
+
